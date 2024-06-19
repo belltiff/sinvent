d39127f556f94d886fac937c4a16f1923ccd0378
root <root@debian.top>
2024-06-19 11:57:18 +0700

    login/logout/register

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,24 @@ use App\Http\Controllers\BarangController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\BarangMasukController;
 use App\Http\Controllers\BarangKeluarController;
-// use App\Http\Controllers\LoginController;
-// use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('barang', BarangController::class);
+Route::resource('barang', BarangController::class)->middleware('auth');
 Route::resource('kategori', KategoriController::class);
 Route::resource('barang_masuk', BarangMasukController::class);
 Route::resource('barang_keluar', BarangKeluarController::class);
+
+Route::get('/login', [LoginController::class,'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class,'authenticate']);
+
+Route::post('logout', [LoginController::class,'logout']);
+Route::get('logout', [LoginController::class,'logout']);
+
+
+Route::post('register', [RegisterController::class,'store']);
+Route::get('/register', [RegisterController::class,'create']);
